686c63703d101db829f0ba94988dfbf5b03a7084
chhabranuj <anujchhabra.work@gmail.com>
2022-04-30 20:58:33 +0530

    make a node server

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+
+let data = [];
+
+app.post('/uploadData', (request, response) => {
+    try {
+        data.push(request.body)
+        response.send({data: data})
+    }
+    catch(error) {
+        console.log(error);
+    }
+})
+
+app.get('/getData', (request, response) => {
+    try {
+        response.send({data: data})
+    }
+    catch(error) {
+        console.log(error);
+    }
+})
+
+app.listen(8000, () => {
+    console.log("Server started.")
+})
